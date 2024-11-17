fde0d0ca31e354ba6361243fae62d6437627f68c
BassemBG <bgbassem8@gmail.com>
2024-11-17 21:40:14 +0100

    admin: filter, order, paginate participations

--- Participation/admin.py
+++ Participation/admin.py
@@ -4,4 +4,14 @@ from .models import participation
 @admin.register(participation)
 class participationAdmin(admin.ModelAdmin):
     list_display = ['participant', 'tournament', 'team_name']
+    
+    # Fields by which the admin can filter the participations
+    list_filter = ('participant', 'tournament', 'team_name')
+    
+    # Fields by which the admin can sort the participations
+    ordering = ('participant', 'tournament', 'team_name')
+    
+    # Number of participations per page
+    list_per_page = 25
+
 # Register your models here.
