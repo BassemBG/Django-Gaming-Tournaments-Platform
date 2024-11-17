4500860ca70c9fd126753e6bc4e53bfee39ec55d
BassemBG <bgbassem8@gmail.com>
2024-11-17 22:15:44 +0100

    added payment model + migration

--- Participation/models.py
+++ Participation/models.py
@@ -10,3 +10,10 @@ class participation(models.Model):
         return self.team_name
     class Meta:
         unique_together = ('participant', 'tournament')
+        
+class Payment(models.Model):
+    participation = models.ForeignKey(participation, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    stripe_payment_intent_id = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, default="pending")
