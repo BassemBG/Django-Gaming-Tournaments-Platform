bedca2db4d45797c8f492afee11280f9cd34b072
AYEDI HEJER <hajer.ayadi@esprit.tn>
2024-11-19 22:38:22 +0100

    clarified dashboard admin

--- equipmentApp/admin.py
+++ equipmentApp/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import Equipment
+from .models import Supplier, Equipment
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('agency_name', 'f_name', 'l_name', 'phone_num', 'address', 'email', 'sup_type', 'created_date', 'is_active')
+    search_fields = ('agency_name', 'f_name', 'l_name', 'email')
+    list_filter = ('sup_type', 'is_active')
+    ordering = ('agency_name',)
 
 @admin.register(Equipment)
 class EquipmentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'type', 'brand', 'stock_quantity', 'condition', 'description', 'supplier', 'updated_at', 'is_available')
+    search_fields = ('name', 'brand', 'type')
+    list_filter = ('condition', 'supplier')
+    ordering = ('name',)
+    readonly_fields = ('updated_at',)
+
+    # Pour afficher si l'équipement est disponible directement dans la liste
+    def is_available(self, obj):
+        return obj.is_available()
+    is_available.boolean = True  # Affiche un icône booléen dans l'interface
+
