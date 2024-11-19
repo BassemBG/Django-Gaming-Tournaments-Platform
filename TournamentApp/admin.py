fd0c3f4c036af9c609fd93e94176c845147afc4d
AYEDI HEJER <hajer.ayadi@esprit.tn>
2024-11-19 22:26:33 +0100

    modified tournamnent-equipment admin dahsboard

--- TournamentApp/admin.py
+++ TournamentApp/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import tournament
+from .models import tournament, TournamentEquipment
 
+# Register the tournament model
 @admin.register(tournament)
-class tournamentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'start_date', 'end_date', 'location', 'status', 'prize', 'price', 'team_size', 'game')
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date', 'location', 'status', 'prize', 'price', 'team_size', 'game')
+    list_filter = ('status', 'game', 'start_date')
+    search_fields = ('name', 'location', 'description')
+
+# Register the TournamentEquipment model
+@admin.register(TournamentEquipment)
+class TournamentEquipmentAdmin(admin.ModelAdmin):
+    list_display = ('tournament', 'equipment', 'quantity')
+    list_filter = ('tournament', 'equipment')
+    search_fields = ('tournament__name', 'equipment__name')
